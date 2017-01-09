5f0af3b048a00394f835036aa4d9aa49c6807db4
Juliane Blier <blier.juliane@gmail.com>
2017-01-09 13:41:35 +0100

    Correction de l'exercice avec isset

--- paramUrl/exercice-1.php
+++ paramUrl/exercice-1.php
@@ -1,5 +1,5 @@
 <?php 
-	if($_GET['nom'] && $_GET['prenom']){
+	if(isset($_GET['nom']) && isset($_GET['prenom'])){
 		echo '<div>Nom : ' .$_GET['nom']. '</div>';
 		echo '<div>Prénom : ' .$_GET['prenom']. '</div>';
 	}
