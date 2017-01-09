4ca9a101389715b7b9b635804921fa393d90f21f
Juliane Blier <blier.juliane@gmail.com>
2017-01-09 13:57:29 +0100

    Formulaire qui redirige vers user.php avec la méthode GET

---
+++ form/exercice-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice 1</title>
+</head>
+<body>
+	<form action="user.php" method="GET">
+		<label for="last_name">Last Name :<input type="text" name="last_name"></label>
+		<label for="first_name">First Name :<input type="text" name="first_name"></label>
+	</form>
+	
+</body>
+</html>
