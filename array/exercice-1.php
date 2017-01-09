e96b6adab2d44f433d92eefb552188f56a8247c7
Juliane Blier <blier.juliane@gmail.com>
2017-01-09 11:46:49 +0100

    Correction de l'accent de Aout

--- array/exercice-1.php
+++ array/exercice-1.php
@@ -1,3 +1,3 @@
 <?php 
-	$mois = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+	$mois = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre');
 	var_dump($mois);
