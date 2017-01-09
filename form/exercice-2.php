c5ef0febeed08dd99b1f12f64ba3315e41fe5284
Juliane Blier <blier.juliane@gmail.com>
2017-01-09 14:00:04 +0100

    Formulaire qui renvoie vers user.php avec la méthode POST

---
+++ form/exercice-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice 2</title>
+</head>
+<body>
+	<form action="user.php" method="POST">
+		<label for="last_name">Last Name :<input type="text" name="last_name"></label>
+		<label for="first_name">First Name :<input type="text" name="first_name"></label>
+		<button type="submit">Send</button>
+	</form>
+	
+</body>
+</html>
