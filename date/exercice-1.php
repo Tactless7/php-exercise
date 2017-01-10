aab0abaf6ffa5e6df89247600791ead0e84f060b
Juliane Blier <blier.juliane@gmail.com>
2017-01-10 09:40:40 +0100

    Affichage de la date sous le format jj/mm/aaaa

---
+++ date/exercice-1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice 1</title>
+</head>
+<body>
+	<h1>Date du jour</h1>
+	<h2> <?= date('d/m/Y')?> </h2>
+	
+</body>
+</html>
