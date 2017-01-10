df62d5c45238655d17c94b5d4373afcba837c339
Juliane Blier <blier.juliane@gmail.com>
2017-01-10 09:55:01 +0100

    Affichage de la date en cours selon la langue locale

---
+++ date/exercice-3.php
@@ -0,0 +1,12 @@
+<?php setlocale(LC_TIME, 'fr_FR.utf8') ?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice 3</title>
+</head>
+<body>
+	<h1>Date en cours</h1>
+	<h2> <?= strftime('%A %e %B %Y') ?> </h2>
+	
+</body>
+</html>
