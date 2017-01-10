6611624b616f8db0d5eef63ac1d519ecc2197dfc
Juliane Blier <blier.juliane@gmail.com>
2017-01-10 09:41:03 +0100

    Affichage de la date courante sous le format jj-mm-aa

---
+++ date/exercice-2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice 2</title>
+</head>
+<body>
+	<h1>Date du jour</h1>
+	<h2> <?= date('d-m-y')?> </h2>
+	
+</body>
+</html>
